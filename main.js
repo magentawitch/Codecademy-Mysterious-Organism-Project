333de9da1f2dc12a4d8b7bd73f87d2e8ba3c9197
Melissa Kuris <melissakuris@hotmail.com>
2024-10-30 16:27:14 -0300

    Update main.js

--- main.js
+++ main.js
@@ -31,9 +31,52 @@ function pAequorFactory(number, array) {
       }
       return array;
     },
+    compareDNA(pAequorObject) {
+      const currentObject = this.specimenNum;
+      const objectToCompare = pAequorObject.specimenNum;
+      let commonDNABase = 0;
+
+      for (let i = 0; i < pAequorObject.dna.length; i++) {
+        if (this.dna[i] === pAequorObject.dna[i]) {
+          commonDNABase += 1;
+        }
+      }
+
+      const percentage = ((commonDNABase * 100)/pAequorObject.dna.length).toFixed(2);
+      console.log(`${currentObject} and ${objectToCompare} share ${percentage}% of DNA.`)
+    },
+    willLikelySurvive() {
+      let containsCOrG = 0;
+
+      for (let i = 0; i < this.dna.length; i++) {
+        if (this.dna[i] === "C" || this.dna[i] === "G") {
+          containsCOrG += 1;
+        }
+      }
+      const porcentage = (containsCOrG * 100)/this.dna.length;
+
+      if (porcentage >= 60) {
+        return true;
+      } else {
+        return false;
+      }
+    },
   }
 }
 
+//pAequor Instances
+let pAequorInstances = [];
+
+for (let i = 30; i > 0; i--) {
+  console.log(i)
+  let object = pAequorFactory(i, mockUpStrand());
+  while (object.willLikelySurvive() === false) {
+    object = pAequorFactory(i, mockUpStrand())
+  }
+  if (object.willLikelySurvive() === true) {
+    pAequorInstances.push(object);
+  }
+}
 
 
 
@@ -49,3 +92,6 @@ let object2 = pAequorFactory(2, mockUpStrand())
 console.log(object2)
 object1.mutate();
 console.log(object2)
+
+object1.compareDNA(object2);
+console.log(object1.willLikelySurvive())
