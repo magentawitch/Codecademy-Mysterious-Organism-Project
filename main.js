eeaf8273369caf08d27942b5f64b6be061f69548
Melissa Kuris <melissakuris@hotmail.com>
2024-10-30 15:25:41 -0300

    Object Factory + Test
    Created an Object Factory function + added some tests

--- main.js
+++ main.js
@@ -13,10 +13,39 @@ const mockUpStrand = () => {
   return newStrand;
 };
 
+//DNA Factory Function
+function pAequorFactory(number, array) {
+  return {
+    specimenNum: number,
+    dna: array,
+    mutate() {
+      let index = Math.floor(Math.random() * 15)
+      let randomBase = array[index]
+      let newBase = returnRandBase();
+
+      while (randomBase === newBase) {
+        newBase = returnRandBase();
+      }
+      if (randomBase !== newBase) {
+        array[index] = newBase;
+      }
+      return array;
+    },
+  }
+}
 
 
 
 
 
 
+//Tests
+let object1 = pAequorFactory(1, mockUpStrand())
+console.log(object1)
+object1.mutate();
+console.log(object1)
 
+let object2 = pAequorFactory(2, mockUpStrand())
+console.log(object2)
+object1.mutate();
+console.log(object2)
